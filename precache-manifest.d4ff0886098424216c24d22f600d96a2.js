1996a14fe13af10201491d5bd00037535e7878ad
Marco Almada <43345162+MarcoDDM@users.noreply.github.com>
2018-12-05 02:53:08 -0300

    Add files via upload

---
+++ precache-manifest.d4ff0886098424216c24d22f600d96a2.js
@@ -0,0 +1,30 @@
+self.__precacheManifest = [
+  {
+    "revision": "12a3db8ba7f5423327bb12aed3c41977",
+    "url": "/static/media/SEGA.12a3db8b.woff"
+  },
+  {
+    "revision": "229c360febb4351a89df",
+    "url": "/static/js/runtime~main.229c360f.js"
+  },
+  {
+    "revision": "f0e3bc932bc8514ab11c",
+    "url": "/static/js/main.f0e3bc93.chunk.js"
+  },
+  {
+    "revision": "290dbb510cdbb5849145",
+    "url": "/static/js/1.290dbb51.chunk.js"
+  },
+  {
+    "revision": "f0e3bc932bc8514ab11c",
+    "url": "/static/css/main.d171697c.chunk.css"
+  },
+  {
+    "revision": "290dbb510cdbb5849145",
+    "url": "/static/css/1.b453c72c.chunk.css"
+  },
+  {
+    "revision": "2595f3e281b6815ec3c429cd8840c7d5",
+    "url": "/index.html"
+  }
+];
